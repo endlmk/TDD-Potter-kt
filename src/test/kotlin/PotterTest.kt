6c63d7880894f02f3e007266607dbc5a276d428c
endlmk <1269467+endlmk@users.noreply.github.com>
2022-11-20 15:10:20 +0900

    Changed gradle based project.

--- src/test/kotlin/PotterTest.kt
+++ src/test/kotlin/PotterTest.kt
@@ -1,9 +1,10 @@
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
-class PotterTest : FunSpec({
+class PotterTest : FunSpec ({
 
     test("my first test") {
         1 + 2 shouldBe 3
     }
+
 })
