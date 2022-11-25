6570f8c0ce057bbbeb8f4437d2d3f364f18e47c5
endlmk <1269467+endlmk@users.noreply.github.com>
2022-11-25 23:01:54 +0900

    Add discount test.

--- src/main/kotlin/Potter.kt
+++ src/main/kotlin/Potter.kt
@@ -1,6 +1,22 @@
 class Potter {
     companion object {
         fun price(books: Array<Int>): Any {
+            val numBooks = mutableMapOf<Int, Int>()
+            for(b in books) {
+                numBooks.put(b, numBooks[b]?.plus(1) ?: 1)
+            }
+            if(numBooks.size == 2) {
+                return 8 * 2 * 0.95
+            }
+            if(numBooks.size == 3) {
+                return 8 * 3 * 0.9
+            }
+            if(numBooks.size == 4) {
+                return 8 * 4 * 0.8
+            }
+            if(numBooks.size == 5) {
+                return 8 * 5 * 0.75
+            }
             return 8 * books.size
         }
     }
--- src/test/kotlin/PotterTest.kt
+++ src/test/kotlin/PotterTest.kt
@@ -4,11 +4,18 @@ import io.kotest.matchers.shouldBe
 class PotterTest : FunSpec ({
 
     test("no discount") {
+        Potter.price(arrayOf(0)) shouldBe 8
         Potter.price(arrayOf(1)) shouldBe 8
         Potter.price(arrayOf(2)) shouldBe 8
         Potter.price(arrayOf(3)) shouldBe 8
         Potter.price(arrayOf(4)) shouldBe 8
-        Potter.price(arrayOf(5)) shouldBe 8
         Potter.price(arrayOf(1, 1, 1)) shouldBe 24
+    }
+
+    test("discount for different books") {
+        Potter.price(arrayOf(1, 2)) shouldBe 8 * 2 * 0.95
+        Potter.price(arrayOf(1, 2, 3)) shouldBe 8 * 3 * 0.9
+        Potter.price(arrayOf(0, 1, 2, 3)) shouldBe 8 * 4 * 0.8
+        Potter.price(arrayOf(0, 1, 2, 3, 4)) shouldBe 8 * 5 * 0.75
     }
 })
