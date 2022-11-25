692aee2a22c012480537ad6efaf77da6d0fc1a85
endlmk <1269467+endlmk@users.noreply.github.com>
2022-11-25 22:09:38 +0900

    Add no discount test.

---
+++ src/main/kotlin/Potter.kt
@@ -0,0 +1,8 @@
+class Potter {
+    companion object {
+        fun price(books: Array<Int>): Any {
+            return 8 * books.size
+        }
+    }
+
+}
--- src/test/kotlin/PotterTest.kt
+++ src/test/kotlin/PotterTest.kt
@@ -3,8 +3,12 @@ import io.kotest.matchers.shouldBe
 
 class PotterTest : FunSpec ({
 
-    test("my first test") {
-        1 + 2 shouldBe 3
+    test("no discount") {
+        Potter.price(arrayOf(1)) shouldBe 8
+        Potter.price(arrayOf(2)) shouldBe 8
+        Potter.price(arrayOf(3)) shouldBe 8
+        Potter.price(arrayOf(4)) shouldBe 8
+        Potter.price(arrayOf(5)) shouldBe 8
+        Potter.price(arrayOf(1, 1, 1)) shouldBe 24
     }
-
 })
