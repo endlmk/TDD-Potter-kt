9425559eadaa9cfec00d43128be6a4678e4a926c
endlmk <1269467+endlmk@users.noreply.github.com>
2022-11-27 21:45:26 +0900

    All test passed.

--- src/main/kotlin/Potter.kt
+++ src/main/kotlin/Potter.kt
@@ -1,3 +1,5 @@
+import kotlin.math.min
+
 class Potter {
     companion object {
         fun price(books: Array<Int>): Any {
@@ -13,11 +15,17 @@ class Potter {
             val pairsOfTwo = numBooks[3] - numBooks[2]
             val singleBooks = numBooks[4] - numBooks[3]
 
+            // replace pairs because two pairs of four is cheaper than a pair of five and a pair of three
+            val replacePairsToTwoFours = min(pairsOfFive, pairsOfThree)
+            val replacedPairsOfFive = pairsOfFive - replacePairsToTwoFours
+            val replacedPairsOfFour = pairsOfFour + replacePairsToTwoFours * 2
+            val replacedPairsOfThree = pairsOfThree - replacePairsToTwoFours
+
             return 8 * singleBooks +
                     (8 * 2 * 0.95) * pairsOfTwo +
-                    (8 * 3 * 0.9) * pairsOfThree +
-                    (8 * 4 * 0.8) * pairsOfFour +
-                    (8 * 5 * 0.75) * pairsOfFive
+                    (8 * 3 * 0.9) * replacedPairsOfThree +
+                    (8 * 4 * 0.8) * replacedPairsOfFour +
+                    (8 * 5 * 0.75) * replacedPairsOfFive
         }
     }
 
--- src/test/kotlin/PotterTest.kt
+++ src/test/kotlin/PotterTest.kt
@@ -25,4 +25,13 @@ class PotterTest : FunSpec ({
         Potter.price(arrayOf(0, 0, 1, 2, 2, 3)) shouldBe (8 * 4 * 0.8) + (8 * 2 * 0.95)
         Potter.price(arrayOf(0, 1, 1, 2, 3, 4)) shouldBe (8 * 5 * 0.75) + 8
     }
+
+    test("edge cases") {
+        Potter.price(arrayOf(0, 0, 1, 1, 2, 2, 3, 4)) shouldBe 2 * (8 * 4 * 0.8)
+        Potter.price(arrayOf(0, 0, 0, 0, 0,
+                             1, 1, 1, 1, 1,
+                             2, 2, 2, 2,
+                             3, 3, 3, 3, 3,
+                             4, 4, 4, 4)) shouldBe 3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8)
+    }
 })
